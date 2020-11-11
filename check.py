265fd9c862dc0c8bb2ed3b2aa8f9b23196fcba07
drZloy <slabengine@gmail.com>
2020-11-11 17:55:02 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,17 +1,4 @@
-# use an appropriate path to import
-from verifier.verifier import Verifier
+<html>
+<meta name="a.validate.01" content="477749fa2960fba2754ae6365664163b508d">
+</html>
 
-# Use normal SMTP to connect to the server
-# normal_verifier = Verifier(source_addr='user@example.com') # No proxy
-# results = normal_verifier.verify('slabengine@gmail.com')
-
-# Use socks proxy to connect over SMTP
-socks_verifier = Verifier(
-    source_addr='noreply@verifyemailaddress.org',
-    proxy_type='socks5',
-    proxy_addr='142.93.51.220',
-    proxy_port=61872,
-)
-response = socks_verifier.verify('test@gmail.com')
-
-print(response)
